20e37662bbfddb018f21385dd72b032329804416
hoang lee <hoanglee@hoangs-MacBook-Air.local>
2020-03-14 01:13:30 +0700

    update

--- hooks/DevServerStart.js
+++ hooks/DevServerStart.js
@@ -1,1 +1,4 @@
-console.log("Started metro server Vn Native .... ");
+let string = '================================================= \n';
+string += ' Started metro server Vn Native .... \n Tác giả : Lê Minh Hoàng \n Liên hệ : 0935 0932 78 \n ';
+string += '================================================= \n';
+console.log(string);
